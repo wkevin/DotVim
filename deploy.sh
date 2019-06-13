e9faf437bbf82aa0551797d637093f165a3b9d2f
wkevin <wkevin27@gmail.com>
2019-06-13 14:09:05 +0800

    增加函数调用关系图的功能，从 github.com/tinyclub/linux-0.11-lab 处copy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-cp .vimrc ~/
+ln -s .vimrc ~/
 vim +BundleInstall +BundleClean! +qa
 cp -r snippets/ ~/.vim/snippets/
 cp -r colors/ ~/DotVim/colors/
+ln -s tools/callgraph ~/bin/
+ln -s tools/tree2dotx ~/bin/
